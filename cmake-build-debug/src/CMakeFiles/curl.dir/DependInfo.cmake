b0225f16863132459d8bb03e75887287701eba56
jcrabb <jamescrabb@wsu.edu>
2021-02-18 14:41:51 -0800

    Catch up fork to curl master

---
+++ cmake-build-debug/src/CMakeFiles/curl.dir/DependInfo.cmake
@@ -0,0 +1,78 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/mnt/e/Classes/423/zerocopy_fork/curl/lib/curl_ctype.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/__/lib/curl_ctype.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/lib/curl_multibyte.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/__/lib/curl_multibyte.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/lib/dynbuf.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/__/lib/dynbuf.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/lib/nonblock.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/__/lib/nonblock.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/lib/strtoofft.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/__/lib/strtoofft.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/lib/version_win32.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/__/lib/version_win32.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/lib/warnless.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/__/lib/warnless.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/slist_wc.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/slist_wc.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_binmode.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_binmode.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_bname.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_bname.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_cb_dbg.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_cb_dbg.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_cb_hdr.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_cb_hdr.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_cb_prg.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_cb_prg.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_cb_rea.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_cb_rea.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_cb_see.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_cb_see.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_cb_wrt.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_cb_wrt.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_cfgable.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_cfgable.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_convert.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_convert.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_dirhie.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_dirhie.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_doswin.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_doswin.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_easysrc.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_easysrc.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_filetime.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_filetime.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_formparse.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_formparse.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_getparam.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_getparam.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_getpass.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_getpass.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_help.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_help.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_helpers.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_helpers.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_homedir.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_homedir.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/tool_hugehelp.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_hugehelp.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_libinfo.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_libinfo.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_main.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_main.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_metalink.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_metalink.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_msgs.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_msgs.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_operate.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_operate.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_operhlp.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_operhlp.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_panykey.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_panykey.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_paramhlp.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_paramhlp.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_parsecfg.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_parsecfg.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_progress.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_progress.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_setopt.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_setopt.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_sleep.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_sleep.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_strdup.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_strdup.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_urlglob.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_urlglob.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_util.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_util.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_vms.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_vms.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_writeout.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_writeout.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_writeout_json.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_writeout_json.c.o"
+  "/mnt/e/Classes/423/zerocopy_fork/curl/src/tool_xattr.c" "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/src/CMakeFiles/curl.dir/tool_xattr.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "HAVE_CONFIG_H"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../lib"
+  "lib"
+  "include"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/mnt/e/Classes/423/zerocopy_fork/curl/cmake-build-debug/lib/CMakeFiles/libcurl.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
